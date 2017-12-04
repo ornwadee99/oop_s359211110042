82ca492206fa1b3d7cca0fa0542ff077800f864c
ornwadee99 <ornwadee28493@gmail.com>
2017-12-04 15:50:48 +0700

    OverloadMethod

---
+++ src/ooplab4/OverloadMethod.java
@@ -0,0 +1,32 @@
+package ooplab4;
+
+public class OverloadMethod {
+    //grobleMethod
+    private static int value =100;
+
+        public static void A(){
+            System.out.println("Hello");
+            int number = 20;
+            System.out.println(number);
+            System.out.println(value);
+        }
+        public static void A (int x){
+            System.out.println("Hello"+x);
+        }
+        public static void A (int x,int y){
+            System.out.println("Hello"+(x+y));
+        }
+
+    public static void main(String[] args) {
+            //OverloadMethod
+        A();
+        A(5);
+        A(5,10);
+
+//      local verriable
+        int number =10;
+
+
+
+    }//main
+}//class
